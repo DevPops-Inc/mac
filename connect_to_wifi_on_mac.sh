2e7bff41264321433c6df1cdbe562b293da0970b
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-03-10 21:09:05 -0800

    Upload SysAdmin scripts.

--- connect_to_wifi_on_mac.sh
+++ connect_to_wifi_on_mac.sh
@@ -4,10 +4,10 @@ set -e
 # connect to Wi-Fi on Mac
 
 #declare variables
-echo "What is SSID you want to connect to?"
-read SSID # Example: "YourWiFi"
-echo "What is the password for the SSID?"
-read PASSWORD # Example: "YourPassword"
+echo "What is SSID you want to connect to? (Example: YourWiFi)"
+read SSID
+echo "What is the password for the SSID? (Example: YourPassword)"
+read PASSWORD
 
 #connect to Wi-Fi
 networksetup -setairportnetwork en0 $SSID $PASSWORD
