c5fb85e76cb07053b7ba7a2b3ac05035eb740e7d
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-04-11 07:45:12 -0700

    Added user prompt.

--- connect_to_wifi_on_mac.sh
+++ connect_to_wifi_on_mac.sh
@@ -3,11 +3,16 @@ set -e
 
 # connect to Wi-Fi on Mac
 
-#declare variables
-echo "What is SSID you want to connect to? (Example: YourWiFi)"
+# prompt user input
+printf "Connect to Wi-Fi on Mac.\n"
+read -p "Press any key to continue press control and C keys to quit."
+printf "\n"
+
+# declare SSID and PASSWORD variables
+printf "What is SSID you want to connect to? (Example: YourWiFi)\n"
 read SSID
-echo "What is the password for the SSID? (Example: YourPassword)"
+printf "What is the password for the SSID? (Example: YourPassword)\n"
 read PASSWORD
 
-#connect to Wi-Fi
+# connect to Wi-Fi
 networksetup -setairportnetwork en0 $SSID $PASSWORD
