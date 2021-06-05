d028a0324e793158eaa87291170a3b2df811fb2b
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-06-05 09:12:51 -0700

    Uploaded copy directory script.

---
+++ copy_directory_on_mac.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+# copy directory on Mac
+
+# prompt user input
+printf "\nCopy directory on Mac."
+read -p "Press any key to continue or press control and C keys to quit."
+
+# declare source and destination variables
+printf "\nWhat is the source directory you wish to copy? (Example: /Documents)"
+read source
+printf "\nWhat is the destination directory you wish to copy to? (Example: /Desktop)"
+read destination
+
+# copy directory
+cp -R $source /$destination
+ls $destination
