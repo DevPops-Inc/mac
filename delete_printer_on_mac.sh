203044ba030619ad0a4f59a89f2c067b794bc539
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-06-07 05:48:21 -0700

    Revised script to be more programmatic.

--- delete_printer_on_mac.sh
+++ delete_printer_on_mac.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
-# delete printer
+# delete printer on Mac
+
+# prompt user input 
+printf "\nDelete printer on Mac."
+read -p "Press any key to continue or press Control and C keys to quit."
 
 # declare variables
-echo "What is name of printer you wish to delete (Example: Printer1)?"
-rad PRINTERNAME
+printf "\nWhat is name of printer you wish to delete (Example: Printer1)?\n"
+read printerName
 
-# delete printer
-/usr/sbin/lpadmin -x $PRINTERNAME
+# define deletePrinter function
+deletePrinter() {
+    # delete printer
+    /usr/sbin/lpadmin -x $printerName
 
-# get printer names
-lpstat -p | awk '{print $2}'
+    # get printer names
+    lpstat -p | awk '{print $2}'
+}
+
+# call deletePrinter function
+deletePrinter
