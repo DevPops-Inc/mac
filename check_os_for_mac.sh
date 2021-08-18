9a402559a4178cf3a06d633607b73ce5163cd8f5
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-08-18 05:58:05 -0700

    Upload checking OS for Mac script.

---
+++ check_os_for_mac.sh
@@ -0,0 +1,19 @@
+#!/bin/bash 
+
+# check OS for Mac
+
+check_os_for_mac() {
+    
+    printf "\nChecking operating system...\n"
+
+    if [[ $OSTYPE == 'darwin'* ]]; then
+        tput setaf 2; echo "Operating System: macOS"; tput sgr0
+        printf "Finished checking operating system.\n"
+    else
+        tput setaf 1; echo "Sorry but this script only works on Mac."; tput sgr0
+        printf "Finished checking operating system.\n"
+        exit 1
+    fi
+}
+
+check_os_for_mac
