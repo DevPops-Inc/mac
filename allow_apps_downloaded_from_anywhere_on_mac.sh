a0137525a2c6a747849bb76c9f749afbe4c5cc74
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-08-21 07:37:57 -0700

    Revised script to be more automated.

--- allow_apps_downloaded_from_anywhere_on_mac.sh
+++ allow_apps_downloaded_from_anywhere_on_mac.sh
@@ -2,12 +2,36 @@
 
 # allow apps downloaded from anywhere on Mac
 
-# prompt user input 
-printf "\nAllow apps to be downloded from anywhere on Mac."
-read -p "Press any key to continue or press control and C keys to quit."
+check_os_for_mac() {
+    
+    printf "\nChecking operating system...\n"
 
-# allow apps downloaded from anywhere on Mac
-sudo spctl --master-disbable 
+    if [[ $OSTYPE == 'darwin'* ]]; then
+        tput setaf 2; echo -e "Operating System: \n$(sw_vers)"; tput sgr0
+        printf "Finished checking operating system.\n\n"
+    else
+        tput setaf 1; echo "Sorry but this script only works on Mac."; tput sgr0
+        printf "Finished checking operating system.\n\n"
+        exit 1
+    fi
+}
 
-# prompt user where to look 
-printf "\nYou will see \"Anywhere\" option under \"Allow apps downloaded from:\" list in Security & Privacy now."
+allow_apps_downloaded_from_anywhere() { 
+    printf "\nAllow apps to be downloded from anywhere on Mac.\n"
+    check_os_for_mac
+
+    start=$(date +%s)
+    printf "Started allowing apps to be downloaded from anywhere...\n"
+    
+    sudo spctl --master-disable 
+
+    printf "\nExpand Apple menu and select \"System Preferences...\"\nSelect \"Security & Privacy\"\nSelect \"General\" tab and you will see \"Anywhere\" option under \"Allow apps downloaded from:\" list now.\nSelect the lock icon and log in with your credentials.\nSelect \"Anywhere\" to allow apps to be downloaded from anywhere on your Mac.\n"
+
+    tput setaf 2; echo -e "\nFinished allowing apps to be downloaded from anywhere."; tput sgr0
+
+    end=$(date +%s)
+    duration=$(( $end - $start ))
+    printf "\nTotal execution time: $duration second(s)"
+}
+
+allow_apps_downloaded_from_anywhere
