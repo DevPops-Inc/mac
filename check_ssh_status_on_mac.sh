0b6c36ce614ab2d4d848da9d0d5ca2cacf11978d
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-08-27 07:08:18 -0700

    Improved logging.

--- check_ssh_status_on_mac.sh
+++ check_ssh_status_on_mac.sh
@@ -5,14 +5,14 @@ set -e
 
 check_os_for_mac() {
     
-    printf "\nChecking operating system...\n"
+    printf "\nChecking operating system at $(date)\n"
 
     if [[ $OSTYPE == 'darwin'* ]]; then
         tput setaf 2; echo -e "Operating System: \n$(sw_vers)"; tput sgr0
-        printf "Finished checking operating system.\n"
+        printf "Finished checking operating system at $(date).\n\n"
     else
         tput setaf 1; echo "Sorry but this script only works on Mac."; tput sgr0
-        printf "Finished checking operating system.\n"
+        printf "Finished checking operating system at $(date).\n\n"
         exit 1
     fi
 }
