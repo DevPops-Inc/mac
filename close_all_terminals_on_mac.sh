0e9aed8711b0be3a4954ae20b8810822a472963f
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-08-31 06:10:33 -0700

    Revised script for better logging.

--- close_all_terminals_on_mac.sh
+++ close_all_terminals_on_mac.sh
@@ -2,9 +2,34 @@
 
 # close all Terminals on Mac
 
-# prompt user input
-printf "\nClose all Terminals on Mac.\n"
-read -p "Press any key any key to continue or press control and C keys to quit."
+check_os_for_mac() {
+    
+    printf "\nChecking operating system...\n"
 
-# close terminals
-ps x | grep Terminal.app | grep -v grep | awk '{ system("kill -9 " $1) }'
+    if [[ $OSTYPE == 'darwin'* ]]; then
+        tput setaf 2; echo -e "Operating System: \n$(sw_vers)"; tput sgr0
+        printf "Finished checking operating system.\n"
+    else
+        tput setaf 1; echo "Sorry but this script only works on Mac."; tput sgr0
+        printf "Finished checking operating system.\n"
+        exit 1
+    fi
+}
+
+close_terminals() { 
+    printf "\nClose All Terminals on Mac.\n"
+    check_os_for_mac
+
+    echo "Started closing terminals at $(date)"
+    start=$(date +%s)
+
+    ps x | grep Terminal.app | grep -v grep | awk '{ system("kill -9 " $1) }'
+
+    tput setaf 2; echo -e "\nFinished closing terminals at $(date)"; tput sgr0
+    end=$(date +%s)
+
+    duration=$(( $end - $start ))
+    printf "\nTotal execution time: $duration second(s)\n"
+}
+
+close_terminals
