b532d29991e4c66985965029e0c5313efe8b5bab
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-09-26 07:58:42 -0700

    Added OS check, parameter checks, execution time and improved exception handling.

--- FileFinder.ps1
+++ FileFinder.ps1
@@ -8,6 +8,24 @@ param(
     , [string] [Parameter(Mandatory = $False)] $filename = ""
 )
 
+function CheckOs()
+{
+    Write-Host "Started checking operating system at" (Get-Date).DateTime
+    $hostOs = [System.Environment]::OSVersion.Platform
+
+    if ($hostOs -eq "Win32NT")
+    {
+        Write-Host "Operating System:" (Get-CimInstance -ClassName Win32_OperatingSystem).Caption -ForegroundColor Green
+    }
+    else 
+    {
+        Write-Host "Operating System:" $hostOs -ForegroundColor Green
+    }
+
+    Write-Host "Finished checking operating system at" (Get-Date).DateTime
+    Write-Host ""
+}
+
 function GetDrive([string]$drive)
 {
     if (($drive -eq $Null) -or ($drive -eq ""))
@@ -36,24 +54,76 @@ function GetFilename([string]$filename)
     }
 }
 
+function CheckParameters([string]$drive, [string]$filename)
+{
+    Write-Host "`nStarted checking parameters at" (Get-Date).DateTime
+    $valid = $True
+
+    Write-Host "`nParameters:"
+    Write-Host "-----------------------------"
+    Write-Host ("drive: {0}" -F $drive)
+    Write-Host ("filename: {0}" -F $filename)
+    Write-Host "-----------------------------"
+
+    if (($drive -eq $Null) -or ($drive -eq ""))
+    {
+        Write-Host "drive is not set." -ForegroundColor Red
+        $valid = $False
+    }
+
+    if (($filename -eq $Null) -or ($filename -eq ""))
+    {
+        Write-Host "filename is not set." -ForegroundColor Red
+        $valid = $False
+    }
+
+    Write-Host "Finished checking parameters at" (Get-Date).DateTime
+
+    if ($valid -eq $True)
+    {
+        Write-Host "All parameter checks passed.`n" -ForegroundColor Green
+    }
+    else 
+    {
+        Write-Host "One or more parameters are incorrect, exiting script." -ForegroundColor Red
+
+        exit -1
+    }
+}
+
 function FileFinder([string]$drive, [string]$filename)
 {    
     Write-Host "`nFile finder.`n"
+    CheckOs
 
     $drive = GetDrive $drive
     $filename = GetFilename $filename
+    CheckParameters $drive $filename
 
     try 
     {
-        Write-Host "File finder starting.`nPlease wait since this may this a while...`n"
+        $startDateTime = (Get-Date)
+
+        Write-Host ("Started finding file {0} in {1} drive at {2}. `nPlease wait since this may this a while...`n" -F $filename, $drive, $startDateTime)
         
         Get-ChildItem -Path $drive -Name *$filename* -Recurse -Force
 
         Write-Host ("Successfully searched for file {0} on {1}" -F $filename, $drive) -ForegroundColor Green
+
+        $finishedDateTime = (Get-Date)        
+        
+        Write-Host ("Finished finding file {0} in {1} drive at {2}" -F $filename, $drive, $finishedDateTime)
+
+        $duration = New-TimeSpan $startDateTime $finishedDateTime
+
+        Write-Host ("Total execution time: {0} hours {1} minutes {2} seconds" -F $duration.Hours, $duration.Minutes, $duration.Seconds)
     }
     catch
     {
-        Write-Host ("Failed searching for file {0} on {1}" -F $filename, $drive) -ForegroundColor Red
+        Write-Host ("Failed searching for file {0} on {1} drive" -F $filename, $drive) -ForegroundColor Red
+
+        Write-Host $_ -ForegroundColor Red
+        Write-Host $_.ScriptStackTrace -ForegroundColor Red
     }
 }
 
