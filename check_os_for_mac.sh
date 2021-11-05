149fdd6c98039bf5a2af34ac144b4f025ddbec6a
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-11-05 07:48:24 -0700

    Improved spacing in script for better readability.

--- check_os_for_mac.sh
+++ check_os_for_mac.sh
@@ -8,12 +8,15 @@ check_os_for_mac() {
 
     if [[ $OSTYPE == 'darwin'* ]]; then
         tput setaf 2; echo -e "Operating System: \n$(sw_vers)"; tput sgr0
+        
         echo "Finished checking operating system at $(date)"
         echo ""
     else
         tput setaf 1; echo "Sorry but this script only runs on Mac."; tput sgr0
+        
         echo "Finished checking operating system at $(date)"
         echo ""
+        
         exit 1
     fi
 }
