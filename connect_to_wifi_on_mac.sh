8bc75db94eebb43458e179173ee04cfe44db0aec
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-11-06 16:25:59 -0700

    Cleaned up code for clarity.

--- connect_to_wifi_on_mac.sh
+++ connect_to_wifi_on_mac.sh
@@ -5,31 +5,37 @@ set -e
 
 # run this script with: ./connect_to_wifi_on_mac.sh < network SSID > < network password > 
 
-ssid=$1
-password=$2
+ssid=$1 # you can set the SSID here 
+password=$2 # you can set the Wi-Fi password here
 
 check_os_for_mac() { 
-    printf "\nStarted checking operating system at $(date)\n"
+    
+    echo "Started checking operating system at $(date)"
 
     if [[ $OSTYPE == 'darwin'* ]]; then 
         tput setaf 2; echo -e "Operating System: \n$(sw_vers)"; tput sgr0
+        
         echo "Finished checking operating system at $(date)"
+        echo ""
     else 
-        tput setaf 1; echo "Sorry but this script only works on Mac."; tput sgr0
+        tput setaf 1; echo "Sorry but this script only runs on Mac."; tput sgr0
+        
         echo "Finished checking operating system at $(date)"
+        echo ""
+
         exit 1
     fi
 }
 
 get_ssid() { 
     if [ -z $ssid ]; then 
-        printf "\nAvailable Wi-Fi networks: \n"
+        echo "Available Wi-Fi networks: "
         
         /System/Library/PrivateFrameworks/Apple80211.framework/Versions/A/Resources/airport scan
 
-        printf "\nPlease type the SSID you wish to connect to and press \"return\" key (Example: YourWiFi): "
+        read -p "Please type the SSID you wish to connect to and press \"return\" key (Example: YourWiFi): " ssid
 
-        read ssid
+        echo ""
     else 
         echo $ssid
     fi 
@@ -37,25 +43,23 @@ get_ssid() {
 
 getPassword() { 
     if [ -z $password ]; then 
-        printf "\nPlease type password for the SSID and press \"return\" key (Example: YourPassword): "
+        read -s "Please type password for the SSID and press \"return\" key (Example: YourPassword): " password
 
-        read -s password
+        echo ""
     else 
         echo $password
     fi
 }
 
 check_parameters() { 
-    printf "\n\nStarted checking parameters at $(date)\n"
+    echo "Started checking parameters at $(date)"
     valid="true"
 
     echo "Parameters:"
-    echo "----------------------------------------"
+    echo "-------------"
     echo "ssid: $ssid"
     echo "password: ***"
-    echo "----------------------------------------"
-
-    echo "Finished checking parameters at $(date)"
+    echo "-------------"
 
     if [ -z $ssid ]; then 
         tput setaf 1; echo "ssid is not set."; tput sgr0
@@ -69,36 +73,39 @@ check_parameters() {
     
     if [ $valid == "true" ]; then 
         tput setaf 2; echo "All parameter checks passed."; tput sgr0
-        echo ""
     else 
-        tput setaf 1; echo "One or more parameters are incorrect, exiting script."; tput sgr0
+        tput setaf 1; echo "One or more parameters are incorrect."; tput sgr0
 
-        echo ""
         exit 1
     fi
+
+    echo "Finished checking parameters at $(date)"
+    echo ""
 }
 
 # define main function 
 connect_to_wifi() { 
-    printf "\nConnect to Wi-Fi on Mac.\n"
+    printf "\nConnect to Wi-Fi on Mac.\n\n"
     check_os_for_mac
 
     get_ssid
     getPassword
     check_parameters
 
-    echo "Started connecting to WiFi at $(date)"
     start=$(date +%s)
+    echo "Started connecting to WiFi at $(date)"
 
     networksetup -setairportnetwork en0 $ssid $password
     
     /System/Library/PrivateFrameworks/Apple80211.framework/Versions/A/Resources/airport scan | grep "$ssid"
 
-    tput setaf 2; echo -e "\nFinished connecting to WiFi at $(date)"; tput sgr0
+    tput setaf 2; echo "Successfully connected to Wi-Fi."; tput sgr0
+    
     end=$(date +%s)
+    echo "Finished connecting to WiFi at $(date)"
 
     duration=$(( $end - $start ))
-    printf "\nTotal execution time: $duration second(s)\n"
+    echo "Total execution time: $duration second(s)"
 }
 
 # call main function 
