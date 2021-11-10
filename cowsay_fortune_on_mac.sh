cf7312db837469653fad211f7462d056896b594f
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-11-10 06:24:42 -0800

    Added cowsay and fortune checks.

--- cowsay_fortune_on_mac.sh
+++ cowsay_fortune_on_mac.sh
@@ -1,16 +1,79 @@
 #!/bin/bash
 
-# prompt user input 
-printf "\nHave the cow say your fortune.\n"
-read -p "Press any key to continue or press control and C keys to quit."
+check_os_for_mac() {
 
-# define cowsayFortune function
-cowsayFortune() {
-    # cowsay fortune
-    printf "\n"
-    fortune | cowsay
-    printf "\n"
+    echo "Started checking operating system at $(date)"
+
+    if [[ $OSTYPE == 'darwin'* ]]; then 
+        tput setaf 2; echo -e "Operating System: \n$(sw_vers)"; tput sgr0
+
+        echo "Finished checking operating system at $(date)"
+        echo ""
+    else 
+        tput setaf 1; echo "Sorry but this script only runs on Mac."; tput sgr0
+
+        echo "Finished checking operating system at $(date)"
+        echo ""
+    fi
 }
 
-# call cowsayFortune function 
-cowsayFortune
+check_fortune() {
+    echo "Started checking fortune at $(date)"
+
+    if [ -d $(which fortune) ]; echo $? == 0 $>/dev/null
+    then 
+        tput setaf 2; echo "fortune is installed."; tput sgr0
+
+        echo "Finished checking fortune at $(date)"
+        echo ""
+    else 
+        tput setaf 1; echo "fortune is not installed."; tput sgr0
+
+        echo "Finished checking fortune at $(date)"
+        echo ""
+    fi
+}
+
+check_cowsay() {
+    echo "Started checking cowsay at $(date)"
+
+    if [ -d $(which cowsay) ]; echo $? == 0 &>/dev/null
+    then 
+        tput setaf 2; echo "cowsay is installed."; tput sgr0
+
+        echo "Finished checking cowsay at $(date)"
+        echo ""
+    else 
+        tput setaf 1; echo "cowsay is not installed."; tput sgr0
+
+        echo "Finished checking cowsay at $(date)"
+        echo ""
+
+        exit 1
+    fi
+}
+
+# define main function
+cowsay_fortune() {
+    printf "\nHave the cow tell your fortune.\n\n"
+    
+    check_os_for_mac
+    check_fortune
+    check_cowsay
+
+    start=$(date +%s)
+    echo "Started cow telling your fortune at $(date)"
+
+    fortune | cowsay
+
+    tput setaf 2; echo "Successfully had cow tell your fortune."; tput sgr0
+
+    end=$(date +%s)
+    echo "Finished cow telling your fortune at $(date)"
+
+    duration=$(( $end - $start ))
+    echo "Total execution time: $duration second(s)"
+}
+
+# call main function 
+cowsay_fortune
