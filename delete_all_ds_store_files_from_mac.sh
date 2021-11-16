c4f32155a0badf47814a2c4c2b41a2ec59de9437
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-11-16 07:09:36 -0800

    Added OS check and main function.

--- delete_all_ds_store_files_from_mac.sh
+++ delete_all_ds_store_files_from_mac.sh
@@ -3,9 +3,39 @@ set -e
 
 # delete all DS_Store files from Mac
 
-# prompt user input
-printf "\nDelete all DS_Store files from Mac."
-read -p "Press any key to continue or press Control and C keys to quit."
+check_os_for_mac() {
 
-# delete all DS_Store files
-find / -name “.DS_Store” -depth -exec rm {} \;
+    echo "Started checking operating system at $(date)"
+
+    if [[ $OSTYPE == 'darwin'* ]]; then 
+        tput setaf 2; echo -e "Operating System: \n$(sw_vers)"; tput sgr0
+
+        echo "Finished checking operating system at $(date)"
+        echo ""
+    else 
+        tput setaf 1; echo "Sorry but this script only runs on Mac."; tput sgr0
+        
+        echo "Finished checking operating system at $(date)"
+        echo ""
+    fi
+}
+
+delete_dsstore_files() {
+    printf "\nDelete all DS_Store files from Mac.\n\n"
+    check_os_for_mac
+
+    start=$(date +%s)
+    echo "Started deleting DS_Store files at $(date)"
+
+    find / -name “.DS_Store” -depth -exec rm {} \;
+
+    tput setaf 2; echo "Successfully deleted DS_Store files."; tput sgr0
+
+    end=$(date +%s)
+    echo "Finished deleting DS_Store files at $(date)"
+
+    duration=$(( $end - $start ))
+    echo "Total execution time: $duration second(s)"
+}
+
+delete_dsstore_files
