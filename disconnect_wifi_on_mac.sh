0630cc3776ec9cf2d4d38afccba609be6e79a91e
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-11-24 20:31:25 -0800

    Added parameters, get functions, parameter checks, and execution time.

--- disconnect_wifi_on_mac.sh
+++ disconnect_wifi_on_mac.sh
@@ -3,20 +3,83 @@ set -e
 
 # disconnect from Wi-Fi on Mac
 
-# prompt user input 
-printf "\nDisconnect from Wi-Fi on Mac."
-read -p "Press any key to continue or press control and C keys to quit."
+# you can run this script with: ./disconnect_wifi_on_mac.sh < SSID > 
 
-# declare ssid variable
-printf "\nwPlease type ssid you wish to disconnect from (Example: IT-Network): "
-read ssid
+ssid=$1 # you can set the SSID you would like to disconnect from here
 
-# define removeWiFiNetwork function
-removeWiFiNetwork() {
-    # remove Wi-Fi network
-    networksetup -removepreferredwirelessnetwork en0 $ssid
-    printf "\nYou are now disconnected from $ssid network.\n"
+check_os_for_mac() {
+
+    echo "Started checking operating system at $(date)"
+
+    if [[ $OSTYPE == 'darwin'* ]]; then 
+        tput setaf 2; echo -e "Operating System: \n$(sw_vers)"; tput sgr0
+
+        echo "Finished checking operating system at $(date)"
+        echo ""
+    else 
+        tput setaf 1; echo "Sorry but this script only runs on Mac."; tput sgr0
+        
+        echo "Finished checking operating system at $(date)"
+        echo ""
+    fi
 }
 
-# call removeWiFiNetwork function
-removeWiFiNetwork
+get_ssid() {
+    if [ -z $ssid ]; then 
+        read -p "Please type ssid you wish to disconnect from (Example: IT-Network): " ssid
+
+        echo ""
+    else 
+        echo $ssid
+    fi
+}
+
+check_parameters() {
+    echo "Started checking parameters at $(date)"
+    valid="true"
+
+    echo "Parameters:"
+    echo "-----------"
+    echo "ssid: $ssid"
+    echo "-----------"
+
+    if [ -z $ssid ]; then 
+        tput setaf 1; echo "ssid is not set."; tput sgr0
+        valid="false"
+    fi
+
+    if [ $valid == "true" ]; then 
+        tput setaf 2; echo "All parameter checks passed."; tput sgr0
+    else 
+        tput setaf 1; echo "One or more parameters are incorrect."; tput sgr0
+        exit 1
+    fi
+
+    echo "Finished checking parameters at $(date)"
+    echo ""
+}
+
+# define main function
+disconnect_wifi() {
+    printf "\nDisconnect from Wi-Fi on Mac.\n\n"
+    check_os_for_mac
+
+    get_ssid
+    check_parameters
+
+    start=$(date +%s)
+    echo "Started disconnecting from $ssid at $(date)"
+
+    networksetup -removepreferredwirelessnetwork en0 $ssid
+    tput setaf 2; echo "Successfully disconnected from $ssid"; tput sgr0
+
+    end=$(date +%s)
+    echo "Finished disconnecting from $ssid at $(date)"
+
+    duration=$(( $end - $start ))
+    echo "Total execution time: $duration second(s)"
+    echo ""
+}
+
+# call main function 
+disconnect_wifi
