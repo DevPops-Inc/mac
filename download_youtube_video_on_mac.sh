73567edb514f9b2e9c3161248c2374acab32e145
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-11-28 11:17:53 -0800

    Added parameters, OS check, get functions, parameter checks, main function, and execution time.

--- download_youtube_video_on_mac.sh
+++ download_youtube_video_on_mac.sh
@@ -2,13 +2,96 @@
 
 # download YouTube video to desktop on Mac
 
-# prompt user input
-printf "\nDownload YouTube video to Desktop on Mac\nYou need youtube-dl installed for this script to work.\n"
-read -p "Press any key to continue or press control and C keys to quit."
+# you can run this script with: ./download_youtube_video_on_mac.sh
 
-# declare youTubeyouTubeUrl variable
-printf "\nCopy the URL of the YouTube video you want and paste it here: \n"
-read youTubeUrl
+youTubeUrl=$1 # you can set the YouTube URL here
 
-# download video from YouTube
-cd ~/Desktop && youtube-dl $youTubeUrl
+check_os_for_mac() {
+
+    echo "Started checking operating system at $(date)"
+
+    if [[ $OSTYPE == 'darwin'* ]]; then
+        tput setaf 2; echo -e "Operating System: \n$(sw_vers)"; tput sgr0
+        
+        echo "Finished checking operating system at $(date)"
+        echo ""
+    else 
+        tput setaf 1; echo "Sorry but this script only runs on Mac."; tput sgr0
+
+        echo "Finished checking operating system at $(date)"
+        echo ""
+    fi 
+}
+
+check_youtube-dl() {
+    echo "Started checking youtube-dl at $(date)"
+
+    if [ -d $(which youtube-dl) ]; echo $? == 0 &>/dev/null
+    then 
+        tput setaf 2; echo "youtube-dl is installed."; tput sgr0
+
+        echo "Finished checking youtube-dl at $(date)"
+        echo ""
+    else 
+        tput setaf 1; echo "youtube-dl is not installed."; tput sgr0
+
+        echo "Finished checking youtube-dl at $(date)"
+        echo ""
+
+        exit 1
+    fi
+}
+
+get_youtube_url() {
+    if [ -z $youTubeUrl ]; then 
+        read -p "Please copy and paste the YouTube URL here then press \"return\" key: " youTubeUrl
+
+        echo ""
+    else 
+        echo $youTubeUrl
+    fi
+}
+
+check_parameters() {
+    echo "Started checking parameters at $(date)"
+    valid="true"
+
+    echo "Parameters:"
+    echo "-----------------------"
+    echo "youTubeUrl: $youTubeUrl"
+    echo "-----------------------"
+
+    if [ -z $youTubeUrl ]; then 
+        tput setaf 1; echo "youTubeUrl is not set."; tput sgr0
+        valid="false"
+    fi
+
+    echo "Finished checking parameters at $(date)"
+    echo ""
+}
+
+download_youtube_video() {
+    printf "\nDownload YouTube video to Desktop on Mac.\n\n"
+
+    check_os_for_mac
+    check_youtube-dl
+
+    get_youtube_url
+    check_parameters
+
+    start=$(date +%s)
+    echo "Started downloading YouTube video at $(date)"
+
+    cd ~/Desktop && youtube-dl $youTubeUrl
+
+    tput setaf 2; echo "Successfully downloaded YouTube video to desktop."; tput sgr0
+
+    end=$(date +%s)
+    echo "Finished downloading YouTube video at $(date)"
+
+    duration=$(( $end - $start ))
+    echo "Total execution time: $duration second(s)"
+    echo ""
+}
+
+download_youtube_video
