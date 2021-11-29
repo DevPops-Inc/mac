a995b2cf4508d1736a04c71c11e02bb5d642add4
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-11-29 06:57:19 -0800

    Added OS check and main function.

--- eject_disc_drive_on_mac.sh
+++ eject_disc_drive_on_mac.sh
@@ -3,9 +3,42 @@ set -e
 
 # eject disc drive on Mac
 
-# prompt user input
-printf "\nEject disc drive on Mac.\n"
-read -p "Press any key to continue or press control and C keys to quit."
+check_os_for_mac() {
 
-# eject disc drive 
-drutil tray eject
+    echo "Started checking operating system at $(date)"
+
+    if [[ $OSTYPE == 'darwin'* ]]; then 
+        tput setaf 2; echo -e "Operating System: \n$(sw_vers)"; tput sgr0
+
+        echo "Finished checking operating system at $(date)"
+        echo ""
+    else 
+        tput setaf 1; echo "Sorry but this script only runs on Mac."; tput sgr0
+
+        echo "Finished checking operating system at $(date)"
+        echo ""
+
+        exit 1
+    fi
+}
+
+eject_disc_drive() {
+    printf "\nEject disc drive on Mac.\n\n"
+    check_os_for_mac
+
+    start=$(date +%s)
+    echo "Started ejecting disc drive at $(date)"
+
+    drutil tray eject
+
+    tput setaf 2; echo "Successfully ejected disc drive."; tput sgr0
+
+    end=$(date +%s)
+    echo "Finished ejecting disc drive at $(date)"
+
+    duration=$(( $end - $start ))
+    echo "Total execution time: $duration seconds"
+    echo ""
+}
+
+eject_disc_drive
