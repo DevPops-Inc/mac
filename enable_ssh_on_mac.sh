28307a234fe8fe58d97462c8e773fd1199ac092f
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-12-03 10:00:24 -0500

    Added execution time to main function.

--- enable_ssh_on_mac.sh
+++ enable_ssh_on_mac.sh
@@ -3,18 +3,44 @@
 # enable SSH on Mac
 
 # prompt user input
-printf "\nEnable SSH on Mac.\n"
-read -p "Press any key to continue or press control and C keys to quit."
 
-# define enableSshOnMac function
-enableSshOnMac() {
-    # check SSH status
-    systemsetup -getremotelogin
+check_os_for_mac() {
 
-    # enable SSH
-    systemsetup -setremotelogin on
-    systemsetup -getremotelogin
+    echo "Started checking operating system at $(date)"
+
+    if [[ $OSTYPE == 'darwin'* ]]; then 
+        tput setaf 2; echo -e "Operating System: \n$(sw_vers)"; tput sgr0
+
+        echo "Finished checking operating system at $(date)"
+        echo ""
+    else 
+        tput setaf 1; echo "Sorry but this script only runs on Mac."; tput sgr0
+        
+        echo "Finished checking operating system at $(date)"
+        echo ""
+
+        exit 1
+    fi
 }
 
-# call enableSshOnMac function
-sudo enableSshOnMac
+enable_ssh() {
+    printf "\nEnable SSH on Mac.\n\n"
+    check_os_for_mac
+
+    start=$(date +%s)
+    echo "Started enabling SSH at $(date)"
+
+    sudo systemsetup -setremotelogin on
+    sudo systemsetup -getremotelogin
+
+    tput setaf 2; echo "Successfully enabled SSH."; tput sgr0
+
+    end=$(date +%s)
+    echo "Finished enabling SSH at $(date)"
+
+    duration=$(( $end - $start ))
+    echo "Total execution time: $duration second(s)"
+    echo ""
+}
+
+enable_ssh
