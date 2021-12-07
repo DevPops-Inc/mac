94ead5db79de83f340569534eb5e4b2e8ccde2e3
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-12-06 18:22:09 -0800

    Cleaned up line spacing.

--- fix_win_bash_script_on_mac.sh
+++ fix_win_bash_script_on_mac.sh
@@ -12,10 +12,12 @@ check_os_for_mac() {
 
     if [[ $OSTYPE == 'darwin'* ]]; then
         tput setaf 2; echo -e "Operating System: \n$(sw_vers)"; tput sgr0
+
         echo "Finished checking operating system at $(date)"
         echo ""
     else 
         tput setaf 1; echo "Sorry but this script only runs on Mac."; tput sgr0
+
         echo "Finished checking operating system at $(date)"
         echo ""
     fi
@@ -90,6 +92,7 @@ fix_win_bash_script() {
 
     duration=$(( $end - $start ))
     echo "Total execution time: $duration second(s)"
+    echo ""
 }
 
 fix_win_bash_script
