da8d4f307062a5fb1a13fbbcb4d7f27196820e2f
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-12-29 08:10:48 -0800

    Uploaded installing Ansible script.

---
+++ install_ansible_with_homebrew.sh
@@ -0,0 +1,60 @@
+#!/bin/bash 
+
+# install Ansible with Homebrew
+
+check_os_for_mac() {
+
+	echo "Started checking operating system at $(date)"
+
+	if [[ $OSTYPE == 'darwin'* ]]; then
+		tput setaf 2; echo -e "Operating System: \n$(sw_vers)"; tput sgr0
+
+		echo "Finished checking operating system at $(date)"
+		echo ""
+	else 
+		tput setaf 1; echo "Sorry but this script only runs on Mac."; tput sgr0
+
+		echo "Finished checking operating system at $(date)"
+		echo ""
+	fi
+}
+
+check_homebrew() {
+	echo "Started checking Homebrew at $(date)"
+
+	which -s brew
+	if [[ $? != 0 ]]; then
+		tput setaf 1; echo "Homebrew needs to be installed."; tput sgr0
+
+		echo "Finished checking Homebrew at $(date)"
+		echo ""
+
+		exit 1
+	else
+		tput setaf 2; echo "Homebrew is installed."; tput sgr0
+
+		echo "Finished checking Homebrew at $(date)"
+		echo ""
+	fi
+}
+
+install_ansible() {
+	echo "Started installing Ansible at $(date)"
+
+	check_os_for_mac
+	check_homebrew
+
+	start=$(date +%s)
+
+	brew install ansible 
+	ansible --version
+
+	tput setaf 2; echo "Finished installing Ansible at $(date)"; tput sgr0
+	end=$(date +%s)
+
+	duration=$(( $end - $start ))
+	echo "Total execution time: $duration second(s)"
+	echo ""
+}
+
+install_ansible
