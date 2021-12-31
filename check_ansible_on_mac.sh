b5dafcb0fa82f7b463ea18e64386ff32294cf343
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2021-12-31 07:45:38 -0800

    Uploaded Ansible scripts.

---
+++ check_ansible_on_mac.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# check Ansible on Mac
+
+check_os_for_mac() {
+
+    echo "Started checking operating system at $(date)"
+
+    if [[ $OSTYPE == 'darwin'* ]]; then
+        tput setaf 2; echo -e "Operating System: \n$(sw_vers)"; tput sgr0
+
+        echo "Finished checking operating system at $(date)"
+        echo ""
+    else 
+        tput setaf 1; echo "Sorry but this script only works on Mac."; tput sgr0
+
+        echo "Finished checking operating system at $(date)"
+        echo ""
+    fi 
+}
+
+check_ansible() {
+    printf "\nCheck Ansible on Mac.\n\n"
+    check_os_for_mac
+
+    start=$(date +%s)
+    echo "Started checking Ansible at $(date)"
+
+    if [ -d $(which ansible) ]; echo $? == 0 &>/dev/null
+    then 
+        tput setaf 2; echo "Ansible is installed."; tput sgr0
+
+        end=$(date +%s)
+        echo "Finished checking Ansible at $(date)"
+
+        duration=$(( $end - $start ))
+        echo "Total execution time: $duration second(s)"
+        echo ""
+    else
+        tput setat 1; echo "Ansible is not installed."; tput sgr0
+
+        end=$(date +%s)
+        echo "Finished checking Ansible at $(date)"
+
+        duration=$(( $end - $start ))
+        echo "Total execution time: $duration second(s)"
+        echo ""
+
+        exit 1
+    fi
+}
+
+check_ansible
---
+++ install_ansible_with_pip_on_mac.sh
@@ -0,0 +1,81 @@
+#!/bin/bash 
+
+# install Ansible with PIP on Mac
+
+check_os_for_mac() {
+
+    echo "Started checking operating system at $(date)"
+
+    if [[ $OSTYPE == 'darwin'* ]]; then
+        tput setaf 2; echo -e "Operating System: \n$(sw_vers)"; tput sgr0
+
+        echo "Finished checking operating system at $(date)"
+        echo ""
+    else 
+        tput setaf 1; echo "Sorry but this script only runs Mac."; tput sgr0
+
+        echo "Finished checking operating system at $(date)"
+        echo ""
+    fi
+}
+
+check_ansible() {
+    echo "Started checking Ansible at $(date)"
+
+    if [ -d $(which ansible) ]; echo $? == 0 &>/dev/null
+    then 
+        tput setaf 2; echo "Ansible is installed."; tput sgr0
+
+        echo "Finished checking Ansible at $(date)"
+        echo ""
+
+        exit 0
+    else 
+        tput setaf 1; echo "Ansible is not installed."; tput sgr0
+
+        echo "Finished checking Ansible at $(date)"
+        echo ""
+    fi
+}
+
+check_pip() {
+    echo "Started checking PIP at $(date)"
+
+    if [ -d $(which pip) ]; echo $? == 0 &>/dev/null
+    then 
+        tput setaf 2; echo "PIP is installed."; tput sgr0
+
+        echo "Finished checking PIP at $(date)"
+        echo ""
+    else
+        tput setaf 1; echo "PIP is not installed."; tput sgr0
+
+        echo "Finished checking PIP at $(date)"
+        echo ""
+
+        exit 1
+    fi
+}
+
+install_ansible() {
+    printf "\nInstall Ansible with PIP on Mac.\n\n"
+    
+    check_os_for_mac
+    check_ansible
+    check_pip
+
+    start=$(date +%s)
+    echo "Started installing Ansible at $(date)"
+
+    pip install ansible 
+    ansible --version 
+
+    end=$(date +%s)
+    echo "Finished installing Ansible at $(date)"
+
+    duration=$(( $end - $start ))
+    echo "Total execution time: $duration second(s)"
+    echo ""
+}
+
+install_ansible
