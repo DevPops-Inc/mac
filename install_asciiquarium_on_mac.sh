e5dcc5fef585488e19e52b3d71323a468eb245e3
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2022-01-03 08:23:32 -0800

    Upload asciiquarium script.

---
+++ install_asciiquarium_on_mac.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+# install asciiquarium with Homebrew
+
+check_os_for_mac() {
+
+    echo "Started checking operating system at $(date)"
+
+    if [[ $OSTYPE == 'darwin'* ]]; then 
+        tput setaf 2; echo -e "Operating System: \n$(sw_vers)"; tput sgr0
+
+        echo "Finished checking operating system at $(date)"
+        echo ""
+    else 
+        tput setaf 1; echo "Sorry but this script only runs on Mac."; tput sgr0
+
+        echo "Finished checking operating system at $(date)"
+        echo ""
+    fi
+}
+
+check_homebrew() {
+    echo "Started checking Homebrew at $(date)"
+
+    which -s brew
+    if [[ $? != 0 ]]; then 
+        tput setaf 1; echo "Homebrew needs to be installed."; tput sgr0
+
+        echo "Finished checking Homebrew at $(date)"
+        echo ""
+
+        exit 1
+    else 
+        tput setaf 2; echo "Homebrew is installed."; tput sgr0
+
+        echo "Finished checking Homebrew at $(date)"
+        echo ""
+    fi
+}
+
+install_asciiquarium() {
+    printf "\nInstall asciiquarium with Homebrew.\n\n"
+
+    check_os_for_mac
+    check_homebrew
+
+    start=$(date +%s)
+    echo "Started installing asciiquarium at $(date)"
+
+    brew install asciiquarium
+
+    tput setaf 2; echo "Succesfully installed asciiquarium."; tput sgr0
+    asciiquarium
+
+    end=$(date +%s)
+    echo "Finished installing asciiquarium at $(date)"
+
+    duration=$(( $end - $start ))
+    echo "Total execution time: $duration second(s)"
+    echo ""
+}
+
+install_asciiquarium
