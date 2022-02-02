b8f14d945277f1a4696febb21c86f264f096d147
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2022-02-02 05:24:45 -0800

    Cleaned up spacing for better readability in the terminal.

--- allow_apps_downloaded_from_anywhere_on_mac.sh
+++ allow_apps_downloaded_from_anywhere_on_mac.sh
@@ -3,23 +3,25 @@
 # allow apps downloaded from anywhere on Mac
 
 check_os_for_mac() {
-    
     echo "Started checking operating system at $(date)"
 
     if [[ $OSTYPE == 'darwin'* ]]; then
         tput setaf 2; echo -e "Operating System: \n$(sw_vers)"; tput sgr0
+        
         echo "Finished checking operating system at $(date)"
         echo ""
     else
         tput setaf 1; echo "Sorry but this script only runs on Mac."; tput sgr0
+        
         echo "Finished checking operating system at $(date)"
         echo ""
+        
         exit 1
     fi
 }
 
 allow_apps_downloaded_from_anywhere() { 
-    printf "\nAllow apps to be downloaded from anywhere on Mac.\n"
+    printf "\nAllow apps to be downloaded from anywhere on Mac.\n\n"
     check_os_for_mac
 
     start=$(date +%s)
@@ -55,6 +57,7 @@ allow_apps_downloaded_from_anywhere() {
 
     duration=$(( $end - $start ))
     echo "Total execution time: $duration second(s)"
+    echo ""
 }
 
 allow_apps_downloaded_from_anywhere
