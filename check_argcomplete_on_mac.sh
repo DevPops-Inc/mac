ec1aefc7d6665a85bde9a8679ed3c9d60ff538f0
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2022-02-06 07:46:45 -0800

    Revised software check logic.

--- check_argcomplete_on_mac.sh
+++ check_argcomplete_on_mac.sh
@@ -3,7 +3,6 @@
 # check argcomplete on Mac
 
 check_os_for_mac() {
-
     echo "Started checking operating system at $(date)"
 
     if [[ $OSTYPE == 'darwin'* ]]; then 
@@ -16,6 +15,8 @@ check_os_for_mac() {
 
         echo "Finished checking operating system at $(date)"
         echo ""
+
+        exit 1
     fi
 }
 
@@ -26,8 +27,17 @@ check_argcomplete() {
     start=$(date +%s)
     echo "Started checking argcomplete at $(date)"
 
-    if [ -d $(which argcomplete) ]; echo $? == 0 &>/dev/null
-    then
+    which -s argcomplete
+    if [[ $? != 0 ]]; then 
+        tput setaf 1; echo "argcomplete is not installed."; tput sgr0
+        
+        end=$(date +%s)
+        echo "Finished checking argcomplete at $(date)"
+
+        duration=$(( $end - $start ))
+        echo "Total execution time: $duration second(s)"
+        echo ""
+    else 
         tput setaf 2; echo "argcomplete is installed."; tput sgr0
 
         end=$(date +%s)
@@ -38,15 +48,6 @@ check_argcomplete() {
         echo ""
 
         exit 0
-    else 
-        tput setaf 1; echo "argcomplete is not installed."; tput sgr0
-        
-        end=$(date +%s)
-        echo "Finished checking argcomplete at $(date)"
-
-        duration=$(( $end - $start ))
-        echo "Total execution time: $duration second(s)"
-        echo ""
     fi
 }
 
