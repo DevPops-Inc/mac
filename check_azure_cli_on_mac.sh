5de3591d0637b4a50c94b97f9f8d9d5d4a048ec1
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2022-02-08 05:33:23 -0800

    Fixed the software check and renamed file.

--- check_azure_cli_on_mac.sh
+++ check_azure_cli_on_mac.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-# check Azure on Mac
+# check Azure CLI on Mac
 
 check_os_for_mac() {
-
 	echo "Started checking operating system at $(date)"
 
 	if [[ $OSTYPE == 'darwin'* ]]; then 
@@ -16,38 +15,40 @@ check_os_for_mac() {
 
 		echo "Finished checking operating system at $(date)"
 		echo ""
+
+		exit 1
 	fi
 }
 
-check_azure() {
-    printf "\nCheck Azure on Mac.\n\n"
+check_azure_cli() {
+    printf "\nCheck Azure CLI on Mac.\n\n"
     check_os_for_mac
 
     start=$(date +%s)
-	echo "Started checking Azure at $(date)"
+	echo "Started checking Azure CLI at $(date)"
 
-	if [ -d $(which az) ]; echo $? == 0 &>/dev/null
-	then 
-		tput setaf 2; echo "Azure is installed."; tput sgr0
-
-        end=$(date +%s)
-		echo "Finished checking Azure at $(date)"
-
-        duration=$(( $end - $start ))
-        echo "Total execution time: $duration second(s)"
-		echo ""
-	else 
-		tput setaf 1; echo "Azure is not installed."; tput sgr0
+	which -s az
+	if [[ $? != 0 ]]; then 
+		tput setaf 1; echo "Azure CLI is not installed."; tput sgr0
 
         end=$(date +%s)
-		echo "Finished checking Azure at $(date)"
+		echo "Finished checking Azure CLI at $(date)"
 
         duration=$(( $end - $start ))
         echo "Total execution time: $duration second(s)"
 		echo ""
 
         exit 1
+	else 
+		tput setaf 2; echo "Azure CLI is installed."; tput sgr0
+
+        end=$(date +%s)
+		echo "Finished checking Azure at $(date)"
+
+        duration=$(( $end - $start ))
+        echo "Total execution time: $duration second(s)"
+		echo ""
 	fi
 }
 
-check_azure
+check_azure_cli
