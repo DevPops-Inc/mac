4c42d2039982394f8342aca295b7c20cd913732e
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2022-02-13 16:06:27 -0800

    Parameterized Android Studio and added logic to support that.

--- check_android_studio_on_mac.sh
+++ check_android_studio_on_mac.sh
@@ -2,6 +2,8 @@
 
 # check Android Studio on Mac
 
+desktopApp="Android Studio"
+
 check_os_for_mac() {
 	echo "Started checking operating system at $(date)"
 
@@ -20,28 +22,66 @@ check_os_for_mac() {
 	fi
 }
 
-check_android_studio() {
-	printf "\nCheck Android Studio on Mac.\n\n"
+get_desktop_app() {
+	if [ -z $desktopApp ]; then 
+		read -p "Please type the name of the desktop app you want to check and prss \"return\" key (Example: Android Studio): " desktopApp
+
+		echo ""
+	else 
+		echo $desktopApp
+	fi
+}
+
+check_parameters() {
+	echo "Started checking parameters at $(date)"
+	valid="true"
+
+	echo "Parameters:"
+	echo "-----------------------"
+	echo "desktopApp: $desktopApp"
+	echo "-----------------------"
+
+	if [ -z "$desktopApp" ]; then 
+		tput setaf 1; echo "desktopApp is not set."; tput sgr0
+		valid="false"
+	fi
+
+	if [ $valid == "true" ]; then 
+		tput setaf 2; echo "All parameter checks passed."; tput sgr0
+	else 
+		tput setaf 1; echo "One or more parameters are incorrect."; tput sgr0
+		exit 1
+	fi
+
+	echo "Finished checking parameters at $(date)"
+	echo ""
+}
+
+check_desktop_app() {
+	printf "\nCheck $desktopApp on Mac.\n\n"
 	check_os_for_mac
 
-	start=$(date +%s)
-	echo "Started checking Android Studio at $(date)"
+	get_desktop_app
+	check_parameters
 
-	if open -Ra "Android Studio"; then
-		tput setaf 2; echo "Android Studio is installed"; tput sgr0
-		open -a "Android Studio.app"
+	start=$(date +%s)
+	echo "Started checking $desktopApp at $(date)"
+
+	if open -Ra "$desktopApp"; then
+		tput setaf 2; echo "$desktopApp is installed"; tput sgr0
+		open -a "$desktopApp.app"
 
 		end=$(date +%s)
-		echo "Finished checking Android Studio at $(date)"
+		echo "Finished checking $desktopApp at $(date)"
 		
 		duration=$(( $end - $start ))
 		echo "Total execution time: $duration second(s)"
 		echo ""
 	else
-		tput setaf 1; echo "Android Studio is not installed"; tput sgr0
+		tput setaf 1; echo "$desktopApp is not installed"; tput sgr0
 		
 		end=$(date +%s)
-		echo "Finished checking Android Studio at $(date)"
+		echo "Finished checking $desktopApp at $(date)"
 		
 		duration=$(( $end - $start ))
 		echo "Total execution time: $duration second(s)"
@@ -51,4 +91,4 @@ check_android_studio() {
 	fi
 }
 
-check_android_studio
+check_desktop_app
