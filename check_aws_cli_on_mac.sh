664be812c560d0f638e98bab6ddb26b78b0f9d58
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2022-02-16 06:11:12 -0800

    Parameterized terminal app and added support logic.

--- check_aws_cli_on_mac.sh
+++ check_aws_cli_on_mac.sh
@@ -2,6 +2,8 @@
 
 # check AWS CLI on Mac
 
+terminalApp="aws"
+
 check_os_for_mac() {
     echo "Started checking operating system at $(date)"
     
@@ -20,37 +22,71 @@ check_os_for_mac() {
     fi
 }
 
-check_aws_cli() {
-    printf "\nCheck AWS CLI on Mac.\n\n"
-    check_os_for_mac
+get_terminal_app() {
+    if [ -z $terminalApp ]; then 
+        read -p "Please type the terminal app you wish to check and press \"return\" key (Example: aws): " terminalApp
 
-    start=$(date +%s)
-    echo "Started checking AWS CLI at $(date)"
-
-    which -s aws
-    if [[ $? != 0 ]]; then 
-        tput setaf 1; echo "AWS CLI is not installed."; tput sgr0
-
-        end=$(date +%s)
-        echo "Finished checking AWS CLI at $(date)"
-
-        duration=$(( $end - $start ))
-        echo "Total execution time: $duration second(s)"
         echo ""
-
     else 
-        tput setaf 2; echo "AWS CLI is installed."; tput sgr0
-        aws --version
-
-        end=$(date +%s)
-        echo "Finished checking AWS CLI at $(date)"
-
-        duration=$(( $end - $start ))
-        echo "Total execution time: $duration second(s)"
-        echo ""
-
-        exit 0
+        echo $terminalApp
     fi
 }
 
-check_aws_cli
+check_parameters() {
+    echo "Started checking parameters at $(date)"
+    valid="true"
+
+    echo "Parameters:"
+    echo "-------------------------"
+    echo "terminalApp: $terminalApp"
+    echo "-------------------------"
+    
+    if [ -z $terminalApp ]; then 
+        tput setaf 1; echo "terminalApp is not set."; tput sgr0
+        valid="false"
+    fi
+
+    if [ $valid == "true" ]; then 
+        tput setaf 2; echo "All parameter checks passed."; tput sgr0
+    else 
+        tput setaf 1; echo "One or more parameter checks are incorrect."; tput sgr0
+        exit 1
+    fi
+
+    echo "Finished checking parameters at $(date)"
+    echo ""
+}
+
+check_terminal_app() {
+    printf "\nCheck $terminalApp on Mac.\n\n"
+    check_os_for_mac
+
+    start=$(date +%s)
+    echo "Started checking $terminalApp at $(date)"
+
+    which -s $terminalApp
+    if [[ $? == 0 ]]; then 
+        tput setaf 2; echo "$terminalApp is installed."; tput sgr0
+        $terminalApp --version
+
+        end=$(date +%s)
+        echo "Finished checking $terminalApp at $(date)"
+
+        duration=$(( $end - $start ))
+        echo "Total execution time: $duration second(s)"
+        echo ""
+    else 
+        tput setaf 1; echo "$terminalApp is not installed."; tput sgr0
+
+        end=$(date +%s)
+        echo "Finished checking $terminalApp at $(date)"
+
+        duration=$(( $end - $start ))
+        echo "Total execution time: $duration second(s)"
+        echo ""
+
+        exit 1
+    fi
+}
+
+check_terminal_app
