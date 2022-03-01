09235778d9f71379ec92b29d11917c87eeb245f0
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2022-02-28 18:11:05 -0800

    Parameterized terminal application and revised logic to match.

--- check_ios-deploy_on_mac.sh
+++ check_ios-deploy_on_mac.sh
@@ -2,52 +2,98 @@
 
 # check ios-deploy on Mac
 
+terminalApp="ios-deploy"
+
 check_os_for_mac() {
-    echo "Started checking operating system at $(date)"
+        echo "Started checking operating system at $(date)"
 
-    if [[ $OSTYPE == 'darwin'* ]]; then 
-            tput setaf 2; echo -e "Operating System: \n$(sw_vers)"; tput sgr0
+        if [[ $OSTYPE == 'darwin'* ]]; then 
+                tput setaf 2; echo -e "Operating System: \n$(sw_vers)"; tput sgr0
 
-            echo "Finished checking operating system at $(date)"
-            echo ""
-    else 
-            tput setaf 1; echo "Sorry but this script only runs on Mac."; tput sgr0
+                echo "Finished checking operating system at $(date)"
+                echo ""
+        else 
+                tput setaf 1; echo "Sorry but this script only runs on Mac."; tput sgr0
 
-            echo "Finisehd checking operating system at $(date)"
-            echo ""
-    fi
+                echo "Finisehd checking operating system at $(date)"
+                echo ""
+
+                exit 1
+        fi
 }
 
-check_ios-deploy() {
-    printf "\nCheck ios-deploy on Mac.\n\n"
-    check_os_for_mac
+get_terminal_app() {
+        if [ -z $terminalApp ]; then 
+                read -p "Please type the terminal application you wish to check and press \"return\" key (Example: ios-deploy): " terminalApp
 
-    start=$(date +%s)
-    echo "Started checking ios-deploy at $(date)"
-
-    if [ -d $(which ios-deploy) ]; echo $? == 0 &>/dev/null
-    then 
-            tput setaf 2; echo "ios-deploy is installed."; tput sgr0
-            ios-deploy --version
-            
-            end=$(date +%s)
-            echo "Finished checking ios-deploy at $(date)"
-
-            duration=$(( $end - $start ))
-            echo "Total execution time: $duration second(s)"
-            echo ""
-
-            exit 0
-    else 
-            tput setaf 1; echo "ios-deploy is not installed."; tput sgr0
-
-            end=$(date +%s)
-            echo "Finished checking ios-deploy at $(date)"
-
-            duration=$(( $end - $start ))
-            echo "Total execution time: $duration second(s)"
-            echo ""
-    fi
+                echo ""
+        else 
+                echo $terminalApp &>/dev/null
+        fi
 }
 
-check_ios-deploy
+check_parameters() {
+        echo "Started checking parameters at $(date)"
+        valid="true"
+
+        echo "Parameters:"
+        echo "-------------------------"
+        echo "terminalApp: $terminalApp"
+        echo "-------------------------"
+
+        if [ -z $terminalApp ]; then 
+                tput setaf 1; echo "terminalApp is not set."; tput sgr0
+                valid="false"
+        fi
+
+        if [ $valid == "true" ]; then 
+                tput setaf 2; echo "All parameter checks passed."; tput sgr0
+
+                echo "Finished checking parameters at $(date)"
+                echo ""
+        else 
+                tput setaf 1; echo "One or more parameters are incorrect."; tput sgr0
+
+                echo "Finished checking parameters at $(date)"
+                echo ""
+        fi
+}
+
+check_terminal_app() {
+        printf "\nCheck $terminalApp on Mac.\n\n"
+        check_os_for_mac
+
+        get_terminal_app
+        check_parameters
+
+        start=$(date +%s)
+        echo "Started checking $terminalApp at $(date)"
+
+        which -s $terminalApp
+        if [ $? == 0 ]; then 
+                tput setaf 2; echo "$terminalApp is installed."; tput sgr0
+                $terminalApp --version
+                tput setaf 2; echo "Successfully checked $terminalApp."; tput sgr0
+                
+                end=$(date +%s)
+                echo "Finished checking $terminalApp at $(date)"
+
+                duration=$(( $end - $start ))
+                echo "Total execution time: $duration second(s)"
+                echo ""
+        else 
+                tput setaf 1; echo "$terminalApp is not installed."; tput sgr0
+                tput setaf 2; echo "Successfully checked $terminalApp."; tput sgr0
+
+                end=$(date +%s)
+                echo "Finished checking $terminalApp at $(date)"
+
+                duration=$(( $end - $start ))
+                echo "Total execution time: $duration second(s)"
+                echo ""
+
+                exit 1
+        fi
+}
+
+check_terminal_app
