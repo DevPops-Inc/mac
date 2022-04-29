d2b0eb31e2a7d49923b427c13114dbc4b7269d0e
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2022-04-29 06:06:59 -0700

    Revised check Homebrew logic.

--- install_ansible_with_homebrew.sh
+++ install_ansible_with_homebrew.sh
@@ -3,7 +3,6 @@
 # install Ansible with Homebrew
 
 check_os_for_mac() {
-
 	echo "Started checking operating system at $(date)"
 
 	if [[ $OSTYPE == 'darwin'* ]]; then
@@ -16,6 +15,8 @@ check_os_for_mac() {
 
 		echo "Finished checking operating system at $(date)"
 		echo ""
+
+		exit 1
 	fi
 }
 
@@ -23,18 +24,18 @@ check_homebrew() {
 	echo "Started checking Homebrew at $(date)"
 
 	which -s brew
-	if [[ $? != 0 ]]; then
+	if [[ $? == 0 ]]; then
+		tput setaf 2; echo "Homebrew is installed."; tput sgr0
+
+		echo "Finished checking Homebrew at $(date)"
+		echo ""
+	else
 		tput setaf 1; echo "Homebrew needs to be installed."; tput sgr0
 
 		echo "Finished checking Homebrew at $(date)"
 		echo ""
 
 		exit 1
-	else
-		tput setaf 2; echo "Homebrew is installed."; tput sgr0
-
-		echo "Finished checking Homebrew at $(date)"
-		echo ""
 	fi
 }
 
@@ -49,8 +50,10 @@ install_ansible() {
 	brew install ansible 
 	ansible --version
 
-	tput setaf 2; echo "Finished installing Ansible at $(date)"; tput sgr0
+	tput setaf 2; echo "Successfully installed Ansible."; tput sgr0
+	
 	end=$(date +%s)
+	echo Finished installing Ansible at $(date)
 
 	duration=$(( $end - $start ))
 	echo "Total execution time: $duration second(s)"
