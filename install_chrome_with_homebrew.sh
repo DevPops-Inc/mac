2660cf509e206c1fed6a4e47f343cd89def02946
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2022-05-05 06:02:32 -0700

    Removed Chrome check and fixed commands in main fucntion.

--- install_chrome_with_homebrew.sh
+++ install_chrome_with_homebrew.sh
@@ -3,7 +3,6 @@
 # install Chrome with Homebrew
 
 check_os_for_mac() {
-
 	echo "Started checking operating system at $(date)"
 
 	if [[ $OSTYPE == 'darwin'* ]]; then 
@@ -16,45 +15,27 @@ check_os_for_mac() {
 
 		echo "Finished checking operating system at $(date)"
 		echo ""
+
+		exit 1
 	fi 
-}
-
-check_chrome() {
-	echo "Started checking Chrome at $(date)"
-
-	if open -Ra "Google Chrome"; then
-		tput setaf 2; echo "Google Chrome is installed."; tput sgr0
-
-		open -a "Google Chrome.app"
-
-		echo "Finished checking Chrome at $(date)"
-		echo ""
-
-		exit 0
-	else
-		tput setaf 2; echo "Google Chrome is not installed."; tput sgr0
-
-		echo "Finished checking Chrome at $(date)"
-		echo ""
-	fi
 }
 
 check_homebrew() {
 	echo "Started checking Homebrew at $(date)"
 
 	which -s brew
-	if [[ $? != 0 ]]; then
+	if [[ $? == 0 ]]; then
+		tput setaf 2; echo "Homebrew is installed."; tput sgr0
+
+		echo "Finished checking Homebrew at $(date)"
+		echo ""
+	else
 		tput setaf 1; echo "Homebrew needs to be installed."; tput sgr0
 
 		echo "Finished checking Homebrew at $(date)"
 		echo ""
 
 		exit 1
-	else
-		tput setaf 2; echo "Homebrew is installed."; tput sgr0
-
-		echo "Finished checking Homebrew at $(date)"
-		echo ""
 	fi
 }
 
@@ -62,15 +43,15 @@ install_chrome() {
 	printf "\nInstall Chrome with Homebrew.\n\n"
 
 	check_os_for_mac
-	check_chrome
 	check_homebrew
 
 	start=$(date +%s)
 	echo "Starting to install Chrome at $(date)"
 
-	brew --cask google-chrome
+	brew install --cask google-chrome
+	/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --version
 
-	tput setaf 2; echo "\nSuccessfully installed Chrome."; tput sgr0
+	tput setaf 2; echo "Successfully installed Chrome."; tput sgr0
 	
 	end=$(date +%s)
 	echo "Finished installing Chrome at $(date)"
