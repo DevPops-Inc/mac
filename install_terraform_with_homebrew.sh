36ea6be010cd5f958f703150f45aac03abd8cb8b
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2022-06-03 05:54:30 -0700

    Removed check Terraform function.

--- install_terraform_with_homebrew.sh
+++ install_terraform_with_homebrew.sh
@@ -4,7 +4,6 @@ set -e
 # install Terraform with Homebrew
 
 check_os_for_mac() {
-
     echo "Started checking operating system at $(date)"
     
     if [[ $OSTYPE == 'darwin'* ]]; then 
@@ -22,43 +21,23 @@ check_os_for_mac() {
     fi
 }
 
-check_terraform() {
-    echo "Started checking Terraform at $(date)"
-
-    if [ -d $(which terraform) ]; echo $? == 0 &>/dev/null
-    then
-        tput setaf 2; echo "Terraform is installed."; tput sgr0
-        terraform --version
-
-        echo "Finished checking Terraform at $(date)"
-        echo ""
-
-        exit 0
-    else 
-        tput setaf 1; echo "Terraform is not installed."; tput sgr0
-
-        echo "Finished checking Terraform at $(date)"
-        echo ""
-    fi
-}
-
 check_homebrew() {
     echo "Started checking Homebrew at $(date)"
 
     which -s brew
-    if [[ $? != 0 ]]; then 
+    if [[ $? == 0 ]]; then 
+        tput setaf 2; echo "Homebrew is installed."; tput sgr0
+        brew --version
+
+        echo "Finished checking Homebrew at $(date)"
+        echo ""
+    else 
         tput setaf 1; echo "Hombrew needs to be installed."; tput sgr0
 
         echo "Finished checking Homebrew at $(date)"
         echo ""
 
         exit 1
-    else 
-        tput setaf 2; echo "Homebrew is installed."; tput sgr0
-        brew --version
-
-        echo "Finished checking Homebrew at $(date)"
-        echo ""
     fi
 }
 
@@ -66,16 +45,15 @@ install_terraform() {
     printf "\nInstall Terraform with Homebrew.\n\n"
 
     check_os_for_mac
-    check_terraform
     check_homebrew
 
     start=$(date +%s)
     echo "Started installing Terraform at $(date)"
 
     brew tap hashicorp/tap
-    brew install terraform
-    tput setaf 2; "Successfully installed Terraform."; tput sgr0
+    brew install hashicorp/tap/terraform
     terraform --version
+    tput setaf 2; echo "Successfully installed Terraform."; tput sgr0
 
     end=$(date +%s)
     echo "Finished installing Terraform at $(date)"
