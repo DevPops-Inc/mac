83832b9f2f9065896b553e676f28d4394ecfccd8
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2022-06-09 05:33:46 -0700

    Rewrote script in functions.

--- kill_dock_on_mac.sh
+++ kill_dock_on_mac.sh
@@ -3,6 +3,41 @@ set -e
 
 # kill dock on Mac
 
-defaults delete com.apple.dock;
+check_os_for_mac() {
+    echo "Started checking operating system at $(date)"
 
-killall Dock
+    if [[ $OSTYPE == 'darwin'* ]]; then 
+        tput setaf 2; echo "Operating system:\n$(sw_vers)"; tput sgr0
+
+        echo "Finished checking operating system at $(date)"
+        echo ""
+    else 
+        tput setaf 1; echo "Sorry but this script only runs on Mac."; tput sgr0
+
+        echo "Finished checking operating system at $(date)"
+        echo ""
+
+        exit 1
+    fi
+}
+
+kill_dock() {
+    printf "\nKill dock on Mac.\n\n"
+    check_os_for_mac
+
+    start=$(date +%s)
+    echo "Started killing dock at $(date)"
+
+    defaults delete com.apple.dock;
+    killall Dock
+    tput setaf 2; echo "Successfully killed dock."; tput sgr0
+
+    end=$(date +%s)
+    echo "Finished killing dock at $(date)"
+
+    duration=$(( $end - $start ))
+    echo "Total execution time: $duration second(s)"
+    echo ""
+}
+
+kill_dock
