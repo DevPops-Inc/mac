823b72ab8ad329d66d9d7f9d7a3c9175d61a7308
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2022-06-24 06:16:21 -0700

    Upload removing file script.

---
+++ remove_file_on_mac.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+set -e
+
+# remove file on Mac
+
+# you can run this script with: ./remove_file_on_mac.sh < file path > 
+
+filePath=$1
+
+check_os_for_mac() {
+    echo "Started checking operating system at $(date)"
+
+    if [[ $OSTYPE == 'darwin'* ]]; then 
+        tput setaf 2; echo -e "Operating system:\n$(sw_vers)"; tput sgr0
+
+        echo "Finished checking operating system at $(date)"
+        echo ""
+    else 
+        tput setaf 1; echo "Sorry but this script only runs on Mac."; tput sgr0
+
+        echo "Finished checking operating system at $(date)"
+        echo ""
+
+        exit 1
+    fi
+}
+
+get_file_path() {
+    if [ -z $filePath ]; then 
+        read -p "Please type the path of the file you wish to remove and press \"return\" key (Example: /Users/$USER/Desktop/foo.txt): " filePath
+
+        echo ""
+    else 
+        echo $filePath &>/dev/null
+    fi
+}
+
+check_parameters() {
+    echo "Started checking parameters at $(date)"
+    valid="true"
+
+    echo "Parameters:"
+    echo "-------------------"
+    echo "filePath: $filePath"
+    echo "-------------------"
+
+    if [ -z $filePath ]; then 
+        tput setaf 1; echo "filePath is not set."; tput sgr0
+        valid="false"
+    fi
+
+    if [ $valid == "true" ]; then 
+        tput setaf 2; echo "All parameter checks passed."; tput sgr0
+
+        echo "Finished checking parameters at $(date)"
+        echo ""
+    else
+        tput setaf 1; echo "One or more parameters are incorrect."; tput sgr0
+
+        echo "Finished checking parameters at $(date)"
+        echo ""
+
+        exit 1
+    fi
+}
+
+remove_file() {
+    printf "\nRemove file on Mac.\n\n"
+    check_os_for_mac
+
+    get_file_path
+    check_parameters
+
+    start=$(date +%s)
+    echo "Started removing file at $(date)"
+
+    printf "Removing: "; rm -v $filePath
+    tput setaf 2; echo "Successfully removed: $filePath"; tput sgr0
+
+    end=$(date +%s)
+    echo "Finisehd removing file at $(date)"
+
+    duration=$(( $end - $start ))
+    echo "Total execution time: $duration second(s)"
+    echo ""
+}
+
+remove_file
