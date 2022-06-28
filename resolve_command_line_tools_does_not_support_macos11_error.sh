a53708bdcadbcd48174109027b5a26c5da694956
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2022-06-28 07:08:44 -0700

    Upload resolving Command Line Tools error script.

--- resolve_command_line_tools_does_not_support_macos11_error.sh
+++ resolve_command_line_tools_does_not_support_macos11_error.sh
@@ -2,11 +2,44 @@
 
 # resolve Command Line Tools does not support macOS 11 error
 
-# update software
-softwareupdate --all --install --force
+# run this script with: sudo /.resolve_command_line_tools_does_not_support_macos11_error.sh
 
-# delete CommandLineTools directory
-sudo rm -rf /Library/Developer/CommandLineTools/
+check_os_for_mac() {
+    echo "Started checking operating system at $(date)"
 
-# reinstall Command Line Tools 
-sudo xcode-select --install
+    if [[ $OSTYPE == 'darwin'* ]]; then 
+        tput setaf 2; echo -e "Operating system:\n$(sw_vers)"; tput sgr0
+
+        echo "Finished checking operating system at $(date)"
+        echo ""
+    else 
+        tput setaf 1; echo "Sorry but this script only runs on Mac."; tput sgr0
+
+        echo "Finished checking operating system at $(date)"
+        echo ""
+
+        exit 1
+    fi
+}
+
+resolve_command_line_tools_error() {
+    printf "\nResolve Command Line Tools does not support macOS 11 error.\n\n"
+    check_os_for_mac
+
+    start=$(date +%s)
+    echo "Started resolving Command Line Tools error at $(date)"
+
+    softwareupdate --all --install --force
+    sudo rm -rf /Library/Developer/CommandLineTools/
+    sudo xcode-select --install
+    tput setaf 2; echo "Successfully resolved Command Line Tools error."; tput sgr0
+
+    end=$(date +%s)
+    echo "Finished resolving Command Line Tools error at $(date)"
+
+    duration=$(( $end - $start ))
+    echo "Total execution time: $duration second(s)"
+    echo ""
+}
+
+resolve_command_line_tools_error
