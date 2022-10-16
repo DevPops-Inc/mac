ebc3661fa4c99d0d6540583dfc93afe7b5fd0c55
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2022-10-16 07:32:24 -0700

    Upload check Rev script for Mac and Linux.

---
+++ checkRevOnMacAndLinux.py
@@ -0,0 +1,80 @@
+#!/bin/python
+
+# check rev on Mac and Linux
+
+import colorama, os, sys, subprocess, traceback
+from colorama import Fore, Style
+from datetime import datetime
+colorama.init()
+
+
+def checkOsForMacOrLinux():
+    print("Started checking operating system at", datetime.now().strftime("%Y-%m-%d %H:%M %p"))
+
+    if sys.platform == "darwin": 
+        print(Fore.GREEN + "Operating System: ")
+        os.system('sw_vers')
+        print(Style.RESET_ALL, end="")
+
+    elif sys.platform == "linux": 
+        print(Fore.GREEN + "Operating System: ")
+        os.system('uname -r')
+        print(Style.RESET_ALL, end="")
+
+    else: 
+        print(Fore.RED + "Sorry but this only runs on Mac or Linux." + Style.RESET_ALL)
+
+        print("Finished checking operating system at", datetime.now().strftime("%Y-%m-%d %H:%M %p)"))
+
+        exit("")
+    
+    print("Finished checking operating system at", datetime.now().strftime("%Y-%m-%d %H:%M %p"))
+    
+    print("")
+
+
+def checkRev(): 
+    print("\nCheck rev on Mac.\n")
+    checkOsForMacOrLinux()
+
+    try:
+        startDateTime = datetime.now()
+        
+        print("Started checking rev at", startDateTime.strftime("%Y-%m-%d %H:%M %p"))
+
+        FNULL = open(os.devnull, 'w')
+
+        checkAnsibleOnMacOrLinux = subprocess.call(['which', 'rev'], stdout=FNULL) 
+
+        if checkAnsibleOnMacOrLinux == 0:
+            print(Fore.GREEN + "rev is installed." + Fore.BLUE)
+            os.system('echo "rev is installed." | rev')
+            print(Fore.GREEN + "Successfully checked rev." + Style.RESET_ALL)
+
+            finishedDateTime = datetime.now()
+
+            print("Finished checking rev at", finishedDateTime.strftime("%Y-%m-%d %H:%M %p"))
+
+            duration = finishedDateTime - startDateTime
+            print("Total execution time: {0} second(s)".format(duration.seconds))
+            print("")
+
+        else: 
+            print(Fore.RED + "rev is not installed." + Style.RESET_ALL)
+            
+            finishedDateTime = datetime.now()
+
+            print("Finished checking rev at", finishedDateTime.strftime("%Y-%m-%d %H:%M %p"))
+
+            duration = finishedDateTime - startDateTime
+            print("Total execution time: {0} second(s)".format(duration.seconds))
+            exit("")
+        
+    except Exception as e: 
+        print(Fore.RED + "Failed to check rev.")
+        print(e)
+        print(traceback.print_stack)
+        exit("" + Style.RESET_ALL)
+
+
+checkRev()
