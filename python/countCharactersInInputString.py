d3c0a9c31514b3271c0afe7a4aa6782cb6819d27
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2022-12-04 15:43:09 -0800

    Fixed comment, parameters, date and time.

--- python/countCharactersInInputString.py
+++ python/countCharactersInInputString.py
@@ -2,62 +2,56 @@
 
 # count characters in input string
 
-# you can run this script with: python3 countCharactersInInputString.py < input string >
+# you can run this script with: python3 countCharactersInInputString.py '< input string >'
 
-import traceback
-import colorama, os, sys 
+import colorama, os, sys, traceback
 from colorama import Fore, Style
 from datetime import datetime
 colorama.init()
 
 
 def checkOs(): 
-    print("Started checking operating system at", datetime.now().strftime("%Y-%m-%d %H:%M %p"))
+    print("Started checking operating system at", datetime.now().strftime("%m-%d-%Y %I:%M %p"))
 
     if sys.platform == "win32": 
-        print(Fore.GREEN + "Operating System")
+        print(Fore.GREEN + "Operating System", end="")
         os.system('ver')
-        print(Style.RESET_ALL)
+        print(Style.RESET_ALL, end="")
         operatingSystem = "Windows"
 
     elif sys.platform == "darwin": 
         print(Fore.GREEN + "Operating System: ")
         os.system('sw_vers')
-        print(Style.RESET_ALL)
+        print(Style.RESET_ALL, end="")
         operatingSystem = "macOS"
 
     elif sys.platform == "linux": 
         print(Fore.GREEN + "Operating System: ")
         os.system('uname -r')
-        print(Style.RESET_ALL)
+        print(Style.RESET_ALL, end="")
         operatingSystem = "Linux"
 
-    print("Finished checking operating system at", datetime.now().strftime("%Y-%m-%d %H:%M %p"))
-
+    print("Finished checking operating system at", datetime.now().strftime("%m-%d-%Y %I:%M %p"))
     print("")
     return operatingSystem
 
 
 def getInputString(operatingSystem): 
     if operatingSystem == "Windows": 
-        inputString = str(input("Please type something you want the character count for and press \"Enter\" key (Example: ham sandwich): "))
-
-        print("")
+        inputString = str(input("Please type something you want the character count for and press \"Enter\" key (Example: Python rocks!): "))
 
     elif operatingSystem == "macOS" or operatingSystem == "Linux": 
-        inputString = str(input("Please type something you want the character count for and press \"return\" key (Example: ham sandwich): "))
+        inputString = str(input("Please type something you want the character count for and press \"return\" key (Example: Python rocks!): "))
 
-        print("")
-
+    print("")
     return inputString
 
 
 def checkParameters(inputString): 
-    print("Started checking parameters at", datetime.now().strftime("%Y-%m-%d %H:%M %p"))
-
+    print("Started checking parameter(s) at", datetime.now().strftime("%m-%d-%Y %I:%M %p"))
     valid = "true"
 
-    print("Parameters:")
+    print("Parameter(s):")
     print("------------------------------------")
     print("inputString: {0}".format(inputString))
     print("------------------------------------")
@@ -67,26 +61,25 @@ def checkParameters(inputString):
         valid = "false"
 
     if valid == "true": 
-        print(Fore.GREEN + "All parameter checks passed." + Style.RESET_ALL)
+        print(Fore.GREEN + "All parameter check(s) passed." + Style.RESET_ALL)
 
-        print("Finished checking parameters at", datetime.now().strftime("%Y-%m-%d %H:%M %p"))
-
+        print("Finished checking parameter(s) at", datetime.now().strftime("%m-%d-%Y %I:%M %p"))
         print("")
 
     else: 
         print(Fore.RED + "One or more parameters are incorrect." + Style.RESET_ALL)
 
-        print("Finished checking parameters at", datetime.now().strftime("Y-%m-%d %H:%M %p"))
-
+        print("Finished checking parameter(s) at", datetime.now().strftime("Y-%m-%d %H:%M %p"))
         exit("")
 
 
 def countCharacters(): 
-    print("\nCount characters in input string.\n")
+    print("\nLet's count characters in an input string in Python.\n")
     operatingSystem = checkOs()
 
     if len(sys.argv) >= 2: 
         inputString = str(sys.argv[1])
+
     else: 
         inputString = getInputString(operatingSystem)
 
@@ -94,20 +87,17 @@ def countCharacters():
 
     try: 
         startDateTime = datetime.now()
+        print("Started counting characters at", startDateTime.strftime("%m-%d-%Y %I:%M %p"))
 
-        print("Started counting characters at", startDateTime.strftime("%Y-%M-%d %H:%M %p"))
+        print(Fore.BLUE + "The string \"{0}\" has {1} characters.".format(inputString, len(inputString)))
 
-        characterCount = len(inputString)
-        print(Fore.BLUE + "The string has {0} characters.".format(characterCount))
-        
         print(Fore.GREEN + "Successfully counted characters in input string." + Style.RESET_ALL)
 
         finishedDateTime = datetime.now()
-
-        print("Finished counting characters at", finishedDateTime.strftime("%Y-%m-%d %H:%M %p"))
+        print("Finished counting characters at", finishedDateTime.strftime("%m-%d-%Y %I:%M %p"))
 
         duration = finishedDateTime - startDateTime
-        print("Total execution time: {0}".format(duration.seconds))
+        print("Total execution time: {0} second(s)".format(duration.seconds))
         print("")
         
     except Exception as e: 
