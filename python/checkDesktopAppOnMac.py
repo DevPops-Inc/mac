5f1bb59da578034d33fb7da148082cf74dee1a49
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2024-12-02 05:47:59 -0800

    Improved exception handling.

--- python/checkDesktopAppOnMac.py
+++ python/checkDesktopAppOnMac.py
@@ -22,11 +22,7 @@ def checkOsForMac():
 
         print("")
     else: 
-        print(Fore.RED + "Sorry this script only runs on macOS." + Style.RESET_ALL)
-
-        print("Finished checking operating system at", datetime.now().strftime("%m-%d-%Y %I:%M %p"))
-
-        exit("")
+        raise Exception("Sorry this script only runs on macOS.")
 
         
 def getDesktopApp(): 
@@ -56,10 +52,7 @@ def checkParameters(desktopApp):
         print("")
 
     else: 
-        print(Fore.RED + "One or more parameters are incorrect." + Style.RESET_ALL)
-
-        print("Finished checking parameter(s) at", datetime.now().strftime("%m-%d-%Y %I:%M %p"))
-        exit("")
+        raise Exception("One or more parameters are incorrect.")
 
 
 def checkDesktopApp():
