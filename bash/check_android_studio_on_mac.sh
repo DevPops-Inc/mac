5d1fbe133bccf8554fb07c0c0f01b017a8f4cc26
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2025-12-26 07:44:51 -0800

    Resolved binary operator warning. Relates to #450.

--- bash/check_android_studio_on_mac.sh
+++ bash/check_android_studio_on_mac.sh
@@ -24,7 +24,7 @@ check_os_for_mac() {
 }
 
 get_desktop_app() {
-	if [ -z $desktopApp ]; then 
+	if [ -z "$desktopApp" ]; then 
 		read -p "Please type the name of the desktop app you want to check and prss \"return\" key (Example: Android Studio): " desktopApp
 
 		echo ""
