30112c59c5afcd1294ba10d7e8687d275cc41b0e
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2026-01-30 07:42:14 -0800

    Wrapped variables in double quotes in get function and parameter checks.

--- bash/check_ios-deploy_on_mac.sh
+++ bash/check_ios-deploy_on_mac.sh
@@ -24,7 +24,7 @@ check_os_for_mac() {
 }
 
 get_terminal_app() {
-        if [ -z $terminalApp ]; then 
+        if [ -z "$terminalApp" ]; then 
                 read -p "Please type the terminal application you wish to check and press \"return\" key (Example: ios-deploy): " terminalApp
 
                 echo ""
@@ -42,7 +42,7 @@ check_parameters() {
         echo "terminalApp: $terminalApp"
         echo "-------------------------"
 
-        if [ -z $terminalApp ]; then 
+        if [ -z "$terminalApp" ]; then 
                 tput setaf 1; echo "terminalApp is not set."; tput sgr0
                 valid="false"
         fi
