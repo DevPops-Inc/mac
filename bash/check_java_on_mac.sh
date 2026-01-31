6dbc5758f69977b263b6aadda76319a2d2675aad
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2026-01-31 08:36:32 -0800

    Improved error handling. Relates to #410.

--- bash/check_java_on_mac.sh
+++ bash/check_java_on_mac.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e 
 
 # check Java on Mac
 
@@ -69,8 +70,7 @@ check_terminal_app() {
     start=$(date +%s)
     echo "Started checking $terminalApp at $(date)"
 
-    which -s $terminalApp
-    if [[ $? == 0 ]]; then
+    if which -s $terminalApp; then
         tput setaf 2; echo "$terminalApp is installed."; tput sgr0
         $terminalApp --version
         tput setaf 2; echo "Successfully checked $terminalApp."; tput sgr0
