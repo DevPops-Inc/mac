514aaed1a5e7ecda3f9e15a8492f27c6459a48b0
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2026-02-05 06:23:57 -0800

    Added get function and parameter checks. Relates to #453.

--- bash/check_jdk_on_mac.sh
+++ bash/check_jdk_on_mac.sh
@@ -3,6 +3,8 @@ set -e
 
 # check JDK on Mac
 
+jdkDir="/Library/Java/JavaVirtualMachines/*jdk*"
+
 check_os_for_mac() {
     echo "Started checking operating system at $(date)"
 
@@ -21,14 +23,54 @@ check_os_for_mac() {
     fi
 }
 
+get_jdk_dir() {
+	if [ -z "$jdkDir" ]; then 
+		read -p "Please type the Java virtual machines directory and press \"return\" key (Example: /Library/Java/JavaVirtualMachines): " jdkDir
+
+		echo ""
+	else 
+		echo $jdkDir &>/dev/null
+	fi
+}
+
+check_parameters() {
+	echo "Started checking parameter(s) at $(date)"
+	valid="true"
+
+	echo "Parameter(s):"
+	echo "---------------"
+	echo "jdkDir: $jdkDir"
+	echo "---------------"
+
+	if [ -z "$jdkDir" ]; then 
+		tput setaf 1; echo "javaVmDir is not set."; tput sgr0
+		valid="false"
+	fi
+
+	if [ $valid == "true" ]; then 
+		tput setaf 2; echo "All parameter check(s) passed."; tput sgr0
+
+		echo "Finished checking parameter(s) at $(date)"
+		echo ""
+	else 
+		tput setaf 1; echo "One or more parameters are incorrect."; tput sgr0
+
+		echo "Finished checking parameter(s) at $(date)"
+		echo ""
+	fi
+}
+
 check_jdk() {
     printf "\nCheck JDK on Mac.\n\n"
     check_os_for_mac
 
+    get_jdk_dir
+    check_parameters
+
     start=$(date +%s)
     echo "Started checking JDK at $(date)"
 
-    if [ -d "/Library/Java/JavaVirtualMachines/*jdk*" ]; echo $? == 0 &>/dev/null
+    if [ -d "$jdkDir" ]; echo $? == 0 &>/dev/null
     then
             tput setaf 2; echo "JDK is installed."; tput sgr0
             java -version
