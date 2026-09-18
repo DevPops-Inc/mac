a6915752201b523598d80053a42ac5e4741939c5
Vic Phan <66754143+VicPhanDevOps@users.noreply.github.com>
2026-09-18 06:59:12 -0700

    Wrapped variables in check in double quotes. Relates to #450.

--- bash/connect_to_wifi_on_mac.sh
+++ bash/connect_to_wifi_on_mac.sh
@@ -11,7 +11,7 @@ password=$2 # you can set the Wi-Fi password here
 check_os_for_mac() { 
     echo "Started checking operating system at $(date)"
 
-    if [[ $OSTYPE == 'darwin'* ]]; then 
+    if [[ "$OSTYPE" == 'darwin'* ]]; then 
         tput setaf 2; echo -e "Operating System: \n$(sw_vers)"; tput sgr0
         
         echo "Finished checking operating system at $(date)"
